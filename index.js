7c806b0332f3b29da062aeeee255de55bb096385
cyberwombat <yashua@urbanlotus.com>
2021-05-19 10:59:53 -0700

    Update

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', "react"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
-    "plugin:react/recommended"
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
   ],
   rules: {
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars-experimental': 'error',
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off'
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
   },
-  "env": {
-    "browser": true,
-    "node": true,
-    "es6": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
   },
-  "settings": {
-    "react": {
-      "version": "detect"
-    }
-  }
-}
-
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
